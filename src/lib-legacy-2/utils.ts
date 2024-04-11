29f177314d2bae1a1d07a57fe928f6fd1de3cfcd
Arthur Gerbelot <arthur.gerbelot@gmail.com>
2024-04-11 08:50:25 -0400

    Remove legacy lib

--- src/lib-legacy-2/utils.ts
+++
@@ -1,7 +0,0 @@
-
-/**
- * Transform a cookies.getAll() to 'classic' string
- */
-export function cookiesToString(cookiesArray: { name: string, value: string }[]): string {
-  return cookiesArray.map(({ name, value }) => `${name}=${value}`).join('; ');
-}
--- src/lib-legacy/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './fetch-api';
-export * from './fetch-rpc';
-
-export * from './fetch-bull';
-export * from './fetch-bull-entity';
-export * from './fetch-bull-list-entities';
